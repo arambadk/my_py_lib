3736154d15873e79b41ea5ca9ca144adde8bc04c
Aditya Ambadkar <arambadk@ncsu.edu>
2016-08-30 16:56:33 -0400

    use list concat vs looping

--- sort/merge.py
+++ sort/merge.py
@@ -13,10 +13,10 @@ def merge(a,b):
                 c.append(b[j])
                 j = j + 1
         elif i == len(a) and j < len(b):
-            c.append(b[j])
-            j = j + 1
+            c = c + b[j:]
+            break
         elif i < len(a) and j == len(b):
-            c.append(a[i])
-            i = i + 1   
+            c = c + a[i:]
+            break
 
     return c
